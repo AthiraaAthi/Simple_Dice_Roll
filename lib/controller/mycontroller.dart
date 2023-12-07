26d3ee6c2b2b66826d248cdba24fcbfd56a59542
AthiraaAthi <athiraa2612@gmail.com>
2023-12-07 08:15:24 +0530

    added isLoading Functionality in Controller

--- lib/controller/mycontroller.dart
+++ lib/controller/mycontroller.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 
 class MyController with ChangeNotifier {
   int randomNumber = 0;
+  bool isLoading = false;
   List diceImages = [
     ImageConstant.dice1,
     ImageConstant.dice2,
@@ -14,6 +15,9 @@ class MyController with ChangeNotifier {
     ImageConstant.dice6
   ];
   generate() {
+    isLoading = true;
+    Future.delayed(Duration(seconds: 3)).then((value) {});
+    //we can also use ; after duration if there's nothing to perform
     randomNumber = Random().nextInt(6);
     //we use random for generating random images(in this case)within the list
     print(randomNumber);
